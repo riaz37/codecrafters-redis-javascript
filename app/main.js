c897a16c1a145850982c1298cd7ee9e4ecd7131c
riaz37 <riaz37.ipe@gmail.com>
2025-03-25 03:59:31 +0600

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,15 +1,14 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
-    connection.addListener('data', (data) => {
-        const dataStr = data.toString();
-        if (dataStr === '*1\r\n$4\r\nping\r\n') {
-          connection.write('+PONG\r\n');
-        }
-    });
+  // Handle incoming data
+  connection.on('data', (data) => {
+    // We don't need to parse the PING command fully for this stage
+    // Just respond with PONG in the correct RESP format
+    connection.write("+PONG\r\n");
+  });
 });
 
 server.listen(6379, "127.0.0.1");
