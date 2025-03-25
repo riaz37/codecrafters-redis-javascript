62994fd8669a63c4ba1ba1939ffddf0ccb5aed51
riaz37 <riaz37.ipe@gmail.com>
2025-03-25 06:05:50 +0600

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -5,9 +5,20 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((connection) => {
   // Handle incoming data
   connection.on('data', (data) => {
-    // We don't need to parse the PING command fully for this stage
-    // Just respond with PONG in the correct RESP format
-    connection.write("+PONG\r\n");
+    const input = data.toString();
+    const parts = input.split('\r\n');
+    
+    // Parse RESP array
+    if (parts[0].startsWith('*')) {
+      const command = parts[2].toUpperCase(); // Command is always the first argument
+      
+      if (command === 'PING') {
+        connection.write("+PONG\r\n");
+      } else if (command === 'ECHO') {
+        const argument = parts[4]; // The argument follows after its length
+        connection.write(`$${argument.length}\r\n${argument}\r\n`);
+      }
+    }
   });
 });
 
